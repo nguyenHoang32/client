e34479fe1080ffbe5088c43c53547c70dd78d9ae
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2020-09-01 23:26:57 +0700

    add client

---
+++ client/build/precache-manifest.394027fc23417e7f7f882a37a1c80cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930d7f1b9e604e9e38d933c3f98d0fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd59a788ca9e519cc6f",
+    "url": "/static/css/main.c68e6461.chunk.css"
+  },
+  {
+    "revision": "314840ff4c2b2364f802",
+    "url": "/static/js/2.a3adb810.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.a3adb810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd59a788ca9e519cc6f",
+    "url": "/static/js/main.e6ab4bc3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
